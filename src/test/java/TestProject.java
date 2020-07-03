8cb88cb5a0a81f11cabafb1e48af8302c337af71
wangcheng <1539113649@qq.com>
2020-07-03 23:15:22 +0800

    wangcheng - 加一个测试类- 20200703

---
+++ src/test/java/TestProject.java
@@ -0,0 +1,20 @@
+/**
+ * @ClassName TestProject
+ * @Description
+ * @Author c_wangcheng-007
+ * @Date 2020/7/3/003 20:50
+ * @Version 1.0
+ **/
+public class TestProject extends DemoApplicationTest {
+
+    /**
+     * @Author c_wangcheng-007
+     * @Description 测试项目
+     * @Date 20:51 2020/7/3/003
+     * @Param []
+     * @return void
+     **/
+    public void testProject(){
+
+    }
+}
