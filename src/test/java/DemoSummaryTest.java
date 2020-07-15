903933b8175c04697f2e40bf8adf199d0d667b22
wangcheng <wangcheng-it@bestpay.com.cn>
2020-07-15 09:31:57 +0800

    wangcheng - 提交类注释模板- 2020-7-15 09:31:50

---
+++ src/test/java/DemoSummaryTest.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ *      演示测试汇总类
+ * </p>
+ *
+ * @author wangcheng
+ * @version Id：DemoSummaryTest.java Date：2020/7/15 9:27 Version：1.0
+ */
+public class DemoSummaryTest extends DemoApplicationTest{
+
+
+}
