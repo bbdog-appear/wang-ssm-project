4b663e65e295b6d35c1d7bccb7c064c5ce532b96
wangcheng <wangcheng-it@bestpay.com.cn>
2020-07-16 18:53:54 +0800

    wangcheng - 测试keys *- 2020-7-16 18:53:44

--- src/test/java/DemoSummaryTest.java
+++ src/test/java/DemoSummaryTest.java
@@ -1,3 +1,9 @@
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Set;
+
 /**
  * <p>
  *      演示测试汇总类
@@ -8,5 +14,14 @@
  */
 public class DemoSummaryTest extends DemoApplicationTest{
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
+    @Test
+    public void testRedisOperate(){
+        redisTemplate.opsForValue().set("wangcheng_1", "value1");
+        redisTemplate.opsForValue().set("wangcheng_2", "value2");
+        Set<String> keys = redisTemplate.keys("wangcheng_*");
+        System.out.println(keys);
+    }
 }
