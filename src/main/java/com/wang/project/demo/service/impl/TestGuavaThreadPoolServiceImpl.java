6a9bfee43e10e669dc3da2165d8e02e6edd23235
wangcheng <wangcheng-it@bestpay.com.cn>
2020-10-13 18:14:35 +0800

    wangcheng - 对于guava线程池和普通线程池的区别：guava多一个回调函数，装入list中批量get，性能方面没有区别- 2020-10-13 18:14:26

--- src/main/java/com/wang/project/demo/service/impl/TestGuavaThreadPoolServiceImpl.java
+++ src/main/java/com/wang/project/demo/service/impl/TestGuavaThreadPoolServiceImpl.java
@@ -1,9 +1,7 @@
 package com.wang.project.demo.service.impl;
 
-import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.wang.project.demo.biz.handle.QueryUserProductHandle;
 import com.wang.project.demo.dao.TestDao;
 import com.wang.project.demo.dao.WcProductDao;
@@ -18,7 +16,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -46,27 +45,79 @@ public class TestGuavaThreadPoolServiceImpl implements TestGuavaThreadPoolServic
     private GuavaThreadPool guavaThreadPool;
     @Autowired
     private QueryUserProductHandle queryUserProductHandle;
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
 
 
+    /**
+     * 结论：guava线程池和普通线程池的区别：其实对于性能来说，两者执行时间差不多，
+     * guava线程池将多个ListenableFuture放入list中，批量get，普通线程池一个一个get，两者的时间差不多。
+     * 但是需要注意的是，普通线程池，必须在所有线程异步执行代码的最后，一个个的get，否则就是线形执行了。
+     *
+     * @throws Exception
+     */
     @Override
     public void testGuavaThreadPool() throws Exception{
 
         UserProductVO userProductVO = new UserProductVO();
 
+//        // 查询产品信息
+//        ListenableFuture<List<WcProductEO>> productListenableFuture = guavaThreadPool.submitAndCallback(() ->
+//                wcProductDao.selectAllWcProductEOs(), queryUserProductHandle.productInfoCallback(userProductVO));
+//        // 查询用户信息
+//        ListenableFuture<List<User>> userListenableFuture = guavaThreadPool.submitAndCallback(() ->
+//                testDao.selectAllUser(), queryUserProductHandle.userInfoCallback(userProductVO));
+
+        // guava线程池执行开始时间
+        long guavaStartMillis = System.currentTimeMillis();
+
         // 查询产品信息
-        ListenableFuture<List<WcProductEO>> productListenableFuture = guavaThreadPool.submitAndCallback(() ->
-                wcProductDao.selectAllWcProductEOs(), queryUserProductHandle.productInfoCallback(userProductVO));
-
+        ListenableFuture<List<WcProductEO>> productListenableFuture = guavaThreadPool.submitAndCallback(() -> {
+            Thread.sleep(2000);
+            return wcProductDao.selectAllWcProductEOs();
+        }, queryUserProductHandle.productInfoCallback(userProductVO));
         // 查询用户信息
-        ListenableFuture<List<User>> userListenableFuture = guavaThreadPool.submitAndCallback(() ->
-                testDao.selectAllUser(), queryUserProductHandle.userInfoCallback(userProductVO));
-
+        ListenableFuture<List<User>> userListenableFuture = guavaThreadPool.submitAndCallback(() -> {
+            Thread.sleep(3000);
+            return testDao.selectAllUser();
+        }, queryUserProductHandle.userInfoCallback(userProductVO));
         ListenableFuture listListenableFuture = Futures.allAsList(productListenableFuture, userListenableFuture);
 
-        listListenableFuture.get(2, TimeUnit.SECONDS);
-
+        listListenableFuture.get(5, TimeUnit.SECONDS);
         System.out.println("================guava线程池获取的结果为：" + userProductVO);
+        // guava线程池执行结束时间
+        long guavaEndMillis = System.currentTimeMillis();
+        // 时间差
+        long guavaResultMillis = guavaEndMillis - guavaStartMillis;
+        System.out.println("guava线程池执行完，耗时：" + guavaResultMillis + "毫秒");
 
+        // 普通线程池执行
+        ordinaryThreadPool();
+    }
+
+    private void ordinaryThreadPool() throws Exception {
+        // 普通线程池执行开始时间
+        long ordinaryStartMillis = System.currentTimeMillis();
+
+        // 普通线程池查询产品信息
+        Future<List<WcProductEO>> submit1 = taskExecutor.submit(() -> {
+            Thread.sleep(2000);
+            return wcProductDao.selectAllWcProductEOs();
+        });
+        // 普通线程池查询用户信息
+        Future<List<User>> submit2 = taskExecutor.submit(() -> {
+            Thread.sleep(3000);
+            return testDao.selectAllUser();
+        });
+        WcProductEO wcProductEO = submit1.get().get(0);
+        User user = submit2.get().get(0);
+        System.out.println("================普通线程池获取的结果为：" + wcProductEO + user);
+
+        // 普通线程池执行结束时间
+        long ordinaryEndMillis = System.currentTimeMillis();
+        // 时间差
+        long ordinaryResultMillis = ordinaryEndMillis - ordinaryStartMillis;
+        System.out.println("普通线程池执行完，耗时：" + ordinaryResultMillis + "毫秒");
     }
 
 }
